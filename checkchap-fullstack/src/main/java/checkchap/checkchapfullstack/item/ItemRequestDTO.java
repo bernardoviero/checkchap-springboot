6921b5d7db48366e9228eba14da6b0eb70dafcd8
Mauricio <mauriciobraga100@gmail.com>
2023-07-02 13:14:54 -0300

    .

--- checkchap-fullstack/src/main/java/checkchap/checkchapfullstack/item/ItemRequestDTO.java
+++ checkchap-fullstack/src/main/java/checkchap/checkchapfullstack/item/ItemRequestDTO.java
@@ -1,7 +1,5 @@
 package checkchap.checkchapfullstack.item;
 
-import checkchap.checkchapfullstack.tarefa.Tarefa;
-
 import java.util.Date;
 
 public record ItemRequestDTO(String item, Date dataModificacao, int Situacao) {
